41f16dbab0515b62809d827a270c601a8d6401c5
Campbell,Ian <icampbell9615@gmail.com>
2019-05-30 17:12:18 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,32 @@
 // JS file for HOMEWORK #5 
 
 $(document).ready(function() {
-    // when the page is loaded first, the timer, questions and summary must be hidden.
+    
     
         $('#countdown').hide();
         $('.trivia-ques').hide();
         $('.results').hide();
     
-        // Declare Variables
-        var number = 90; //number of seconds
+    
+        var number = 90; 
         var intervalId;
         var correctCount = 0;
         var wrongCount = 0;
         var unanswered = 0;
     
-    //ALL FUNCTIONS
-    
-        // function to show questions
+   
         function showQuestions(){
             $('#countdown').show();
             $('.trivia-ques').show();
             $('#game-done').show();
         }
     
-        // function for timer
+       
         function countdownTimer(){
                 intervalId = setInterval(decrement, 1000);
         }
        
-            // function to decrement timer
+           
         function decrement(){
             number--;
             $('#timer').html(" " + number + " " + "seconds");
@@ -42,19 +40,19 @@ $(document).ready(function() {
             }
         }
     
-            //function to clear timer
+           
         function stop() {
             clearInterval(intervalId);
         }
     
-            //function to hide text after questions are answered or timer out
+            
         function hide(){
             $('#countdown').hide();
             $('.trivia-ques').hide();
             $('#game-done').hide();
         }
     
-        // function to display summary of game
+       
         function displaySummary(){
             $('.results').show();
             unanswered = (8-(correctCount+wrongCount));
@@ -63,28 +61,26 @@ $(document).ready(function() {
             $('#unanswered').text("Unanswered:" + " " + unanswered); 
         }
     
-    // CLICK EVENTS
     
-        //Clicking Start Button
         $('#game-start').on('click', function(){
             $('#game-start').hide();
             showQuestions();
             countdownTimer();
         }); 
     
-        //Clicking Done Button
+        
         $('#game-done').on('click', function(){
             $('#game-start').hide(); 
             hide();
             displaySummary();
         });
     
-        //Clicking Radio button
+    
         $('input[type=radio]').on ('change', function(){
         correctCount = $('input[value=correct]:checked').length;
         wrongCount = $('input[value=wrong]:checked').length;
         unanswered = (8-(correctCount+wrongCount));
         });
     
-    // Last closing bracket
+  
     });
